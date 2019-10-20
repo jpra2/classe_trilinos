c4fb3bac9803802066e408766616fbd62bcb3630
jprandrade <jprandrade2@gmail.com>
2019-10-20 11:24:26 -0300

    testting1

--- classes/preprocess/trilinos_init.py
+++ classes/preprocess/trilinos_init.py
@@ -1,14 +1,27 @@
-from PyTrilinos import Epetra, AztecOO, EpetraExt
+from PyTrilinos import Epetra, AztecOO, EpetraExt, Teuchos
 import numpy as np
 import scipy.sparse as sp
 
 
-class TrilInit:
+class PyTrilWrap:
 
     def __init__(self, p=1):
-        self.comm  = Epetra.PyComm()
+        self.__comm  = Epetra.PyComm()
+        self.__params = dict()
+        self.set_parameters()
+        help(Teuchos)
+        import pdb; pdb.set_trace()
 
-    def solve_linear_problem(self, A, b):
+    def solve_linear_problem(self, A, b, its=1000, tolerance=1e-10):
+        '''
+        resolve o problema Ax = b
+        input:
+            A: matriz quadrada do scipy
+            b = termo fonte
+        output:
+            res: informa se o residuo foi menor que a tolerancia
+            x: vetor resposta
+        '''
         comm = self.comm
         n = len(b)
         std_map = Epetra.Map(n, 0, comm)
@@ -18,10 +31,21 @@ class TrilInit:
         A2 = Epetra.CrsMatrix(Epetra.Copy, std_map, 7)
         indices = sp.find(A)
         A2.InsertGlobalValues(indices[0], indices[1], indices[2])
+        irr = A2.FillComplete()
         linearProblem = Epetra.LinearProblem(A2, x, b2)
         solver = AztecOO.AztecOO(linearProblem)
         solver.SetAztecOption(AztecOO.AZ_output, AztecOO.AZ_warnings)
-        solver.Iterate(1000, 1e-12)
+        solver.SetParameters(self.__params)
+        solver.Iterate(its, tolerance)
         x = np.array(x)
+        res = solver.ScaledResidual() < tolerance
+        return x, res
 
-        return x
+    def set_parameters(self, params=None):
+        if params:
+            pass
+        else:
+            params = {'Solver': 'GMRES',
+                      'Precond': 'Jacobi'}
+
+        self.__params.update(params)
---
+++ testting.py
@@ -0,0 +1,2 @@
+from classes.preprocess.trilinos_init import PyTrilWrap
+tril = PyTrilWrap()
